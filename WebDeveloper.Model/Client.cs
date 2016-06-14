c210e22aa5141155994770a493c245e1d943bf52
Hernandez Celiz Cesar J. <jhon_15_1610@hotmail.com>
2016-06-13 22:28:31 -0500

    13062016:Creacion Code First EntityFrawork

--- WebDeveloper.Model/Client.cs
+++ WebDeveloper.Model/Client.cs
@@ -4,11 +4,9 @@ namespace WebDeveloper.Model
     public class Client
     {
         public int ID { get; set; }
-
         [Display(Name = "First Name")]
         [Required(ErrorMessage = "This field is required")]
         public string Name { get; set; }
-
         [Display(Name = "Last Name")]
         [Required(ErrorMessage = "The Last Name is required")]
         public string LastName { get; set; }
