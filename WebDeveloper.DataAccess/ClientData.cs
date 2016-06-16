792a0812dcb324cdbb2823d3af59d45b53cb5b7d
Hernandez Celiz Cesar J. <jhon_15_1610@hotmail.com>
2016-06-15 20:48:27 -0500

    Implementacion operaciones CRUD

--- WebDeveloper.DataAccess/ClientData.cs
+++ WebDeveloper.DataAccess/ClientData.cs
@@ -5,22 +5,8 @@ using WebDeveloper.Model;
 
 namespace WebDeveloper.DataAccess
 {
-    public class ClientData
+    public class ClientData:BaseDataAccess<Client>
     {
-        public List<Client> GetFakeData()
-        {
-            return new List<Client>
-            {
-                new Client { ID=1, Name ="Juan", LastName ="Perez"},
-                new Client { ID=1, Name ="Raul", LastName ="Ruidiaz"}
-            };
-        }
-        public List<Client> GetList()
-        {
-            using (var dbContext = new WebContextDb())
-            {
-                return dbContext.Clients.ToList();
-            }
-        }
+
     }
 }
--- WebDeveloper/Controllers/ClientController.cs
+++ WebDeveloper/Controllers/ClientController.cs
@@ -1,19 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using WebDeveloper.DataAccess;
+using WebDeveloper.Model;
 
 namespace WebDeveloper.Controllers
 {
     public class ClientController : Controller
     {
+        private ClientData _client = new ClientData();
         // GET: Client
         public ActionResult Index()
         {
-            var Client = new ClientData();
-            return View(Client.GetList());
+            return View(_client.GetList());
+        }
+        public ActionResult Create()
+        {
+            return View(new Client());
+        }
+        [HttpPost]
+        public ActionResult Create(Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                _client.Add(client);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Client client = _client.GetList().Find(x => x.ID == id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            return View(client);
+        }
+        [HttpPost]
+        public ActionResult Edit(Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                _client.Update(client);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Client client = _client.GetList().Find(x => x.ID == id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            return View(client);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Client client)
+        {
+            _client.Delete(client);
+            return RedirectToAction("Index");
         }
     }
 }
