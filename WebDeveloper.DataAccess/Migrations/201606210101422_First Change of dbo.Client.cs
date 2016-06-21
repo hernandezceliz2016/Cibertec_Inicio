d9ffd60ff8134fe4a677cc6878c3d99cfcf4fc6f
Hernandez Celiz Cesar J. <jhon_15_1610@hotmail.com>
2016-06-20 20:37:26 -0500

    Migrations 20-jun-2016

--- WebDeveloper.DataAccess/Migrations/201606210101422_First Change of dbo.Client.cs
+++ WebDeveloper.DataAccess/Migrations/201606210101422_First Change of dbo.Client.cs
@@ -8,15 +8,13 @@ namespace WebDeveloper.DataAccess.Migrations
         public override void Up()
         {
             RenameColumn("dbo.Client", "Apellidos", "LastName");
-            //AddColumn("dbo.Client", "LastName", c => c.String(nullable: false));
-            //DropColumn("dbo.Client", "Apellidos");
+           
         }
         
         public override void Down()
         {
             RenameColumn("dbo.Client", "LastName","Apellidos");
-            //AddColumn("dbo.Client", "Apellidos", c => c.String(nullable: false));
-            //DropColumn("dbo.Client", "LastName");
+      
         } 
     }
 }
